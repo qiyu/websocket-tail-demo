1f5c72e83a97eb51966d6dafd8c89db2ab8da04b
yu.qi <yu.qi@qunar.com>
2018-06-20 23:37:50 +0800

    代码格式化，重构

--- src/main/java/com/xxg/websocket/TailLogThread.java
+++ src/main/java/com/xxg/websocket/TailLogThread.java
@@ -1,33 +1,44 @@
 package com.xxg.websocket;
 
+import javax.websocket.Session;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import javax.websocket.Session;
-
 public class TailLogThread extends Thread {
-	
-	private BufferedReader reader;
-	private Session session;
-	
-	public TailLogThread(InputStream in, Session session) {
-		this.reader = new BufferedReader(new InputStreamReader(in));
-		this.session = session;
-		
-	}
-	
-	@Override
-	public void run() {
-		String line;
-		try {
-			while((line = reader.readLine()) != null) {
-				// 将实时日志通过WebSocket发送给客户端，给每一行添加一个HTML换行
-				session.getBasicRemote().sendText(line + "<br>");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+
+    private BufferedReader reader;
+    private Session session;
+
+
+    public TailLogThread(InputStream in, Session session) {
+        setDaemon(true);
+        setName("TailLogThread");
+        this.reader = new BufferedReader(new InputStreamReader(in));
+        this.session = session;
+
+    }
+
+    @Override
+    public void run() {
+        int maxSize = 1024;
+        char[] buffer = new char[maxSize];
+        try {
+            while (true) {
+                int size = reader.read(buffer);
+                if (size == 0) {
+                    break;
+                }
+                // 将实时日志通过WebSocket发送给客户端，给每一行添加一个HTML换行
+                session.getBasicRemote().sendText(String.copyValueOf(buffer));
+                // 防止日志加载过快，影响客户端性能
+                if (size == maxSize) {
+                    Thread.sleep(100);
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
